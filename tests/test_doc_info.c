a55c588200d78679386d66ce575bd9f7719d8376
Joshua Weinstein <jweinstein@splunk.com>
2018-12-16 21:11:39 -0800

    first test

--- tests/test_doc_info.c
+++ tests/test_doc_info.c
@@ -6,10 +6,15 @@
 
 void test_doc_header_check_magic(void)
 {
-	
+	dr4_doc_header_t a;
+	a.magic[0] = 83;
+	a.magic[1] = 94;
+	a.magic[2] = 121;
+	TEST_FAIL_CHECK(doc_header_check_magic(&a));
 }
 
 int main(int argc, char const *argv[])
 {
+	test_doc_header_check_magic();
 	TEST_FAIL_RETURN
 }
