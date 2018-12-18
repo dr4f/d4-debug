88c8b9cae59c785899c83f61e7a7b470aab49252
Joshua Weinstein <jweinstein@splunk.com>
2018-12-18 09:48:21 -0800

    finished test helper io lib

--- tests/io_test_help.c
+++ tests/io_test_help.c
@@ -3,8 +3,46 @@
 /* If no directory is defined, the working directory is used.
  */
 #ifdef DR4_DEBUG_TDIR
-    static char IO_TEST_PATH_BIN[1024] = DR4_DEBUG_TDIR;
+    static char IO_TEST_PATH_BIN[IO_TEST_PATH_SZ] = DR4_DEBUG_TDIR;
 #else
-    static char IO_TEST_PATH_BIN[1024];
+    static char IO_TEST_PATH_BIN[IO_TEST_PATH_SZ];
 #endif
+
+const char* io_test_get_path(void)
+{
+	return IO_TEST_PATH_BIN;
+}
+
+void io_test_set_path(const char* file_name)
+{
+	(void) strncat(IO_TEST_PATH_BIN, file_name, IO_TEST_PATH_SZ);
+}
+
+void io_test_remove_path(void)
+{
+	remove(IO_TEST_PATH_BIN);
+}
+
+int io_test_real_dump(void* data, size_t n)
+{
+	FILE* fp;
+	fp = fopen(IO_TEST_PATH_BIN, "wb");
+	if(fp == NULL) return 0;
+	fwrite(data, n, 1, fp);
+	fclose(fp);
+	return 1;
+}
+
+FILE* io_test_real_open(void)
+{
+	return fopen(IO_TEST_PATH_BIN, "rb");
+}
+
+FILE* io_test_dump_fake(void* data, size_t n)
+{
+	FILE* fp = tmpfile();
+	fwrite(data, n, 1, fp);
+	rewind(fp);
+	return fp;
+}
 
--- tests/io_test_help.h
+++ tests/io_test_help.h
@@ -2,13 +2,45 @@
 #define IO_TEST_HELP_H
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-
+/**
+ * The max size of a test file path. 
+ */
+#define IO_TEST_PATH_SZ 1024
 /**
  * Contains methods that help facilitate file dumping and writing
  * to specific paths in C.
  */
 
+/* Get the current full path of the test file,
+ * which has the testing dir prepended to it.
+ */
+extern const char* io_test_get_path(void);
+
+/* Given the testing dir, append a file name with a '/' sep
+ * to the test file path.
+ */
+extern void io_test_set_path(const char* file_name);
+/*
+ * Removes file in testing dir.
+ */
+extern void io_test_remove_path(void);
+/*
+ * Dumps data into a real file in the testing dir.
+ */
+extern int io_test_real_dump(void* data, size_t n);
+
+/* 
+ * Opens the file at path IO_TEST_PATH_BIN
+ */
+extern FILE* io_test_real_open(void);
+/*
+ * Instead of opening the io_test_path_bin file, this uses tmpfile() to create
+ * a fake file pointer, only usable for testing purposes. It also dumps data into
+ * the tmpfile and rewinds.
+ */
+extern FILE* io_test_dump_fake(void* data, size_t n);
 
 #endif // IO_TEST_HELPERS_H
